c852c1d68ef0d0189e313f0322910228e0884527
nri <47117716+nri-1@users.noreply.github.com>
2019-05-16 18:42:17 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,8 +3,8 @@ minetest.register_tool("spawn_teleporter:spawn_tool", {
     inventory_image = "spawn_teleporter.png",
     on_punch = function(pos, node, puncher)
         local player_name = puncher:get_player_name()
-   end,
-local punched_player_privs = minetest.send_all(punched_player)
+    end,
+    local punched_player_privs = minetest.send_all(punched_player)
 })
 
 

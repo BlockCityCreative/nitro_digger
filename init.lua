b7ea062224bd0af8c327bbac740118c165ea50d2
nri <47117716+nri-1@users.noreply.github.com>
2019-05-16 18:40:52 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,10 @@
 minetest.register_tool("spawn_teleporter:spawn_tool", {
     description = "Tool that is used to teleport people back to the spawn",
     inventory_image = "spawn_teleporter.png",
-})
-local punched_player = get_player_name()
+    on_punch = function(pos, node, puncher)
+        local player_name = puncher:get_player_name()
+   end,
 local punched_player_privs = minetest.send_all(punched_player)
+})
+
 

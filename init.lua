b93a77a477a9f7a0be583457724112deedf02fa4
nri <47117716+nri-1@users.noreply.github.com>
2019-05-17 17:06:43 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,37 +7,44 @@ minetest.register_tool("nitro_digger:nitro_digger", {
         groupcaps = {
             crumbly = {
                 maxlevel = 1,
-                uses = 20,
+                uses = 900,
                 times = { [1]=0.15, [2]=0.15, [3]=0.15 }
             },
             snappy = {
                 maxlevel = 1,
-                uses = 9000,
+                uses = ,
                 times = { [1]=0.15, [2]=0.15, [3]=0.15 }
             },
 	    choppy = {
                 maxlevel = 1,
-                uses = 9000,
+                uses = 900,
                 times = { [1]=0.15, [2]=0.15, [3]=0.15 }
             },
 	    fleshy = {
                 maxlevel = 1,
-                uses = 9000,
+                uses = 900,
                 times = { [1]=0.15, [2]=0.15, [3]=0.15 }
             },
 	   explody = {
                 maxlevel = 1,
-                uses = 9000,
+                uses = 900,
                 times = { [1]=0.15, [2]=0.15, [3]=0.15 }
             },
 	    oddly_breakable_by_hand = {
                 maxlevel = 1,
-                uses = 9000,
+                uses = 900,
                 times = { [1]=0.15, [2]=0.15, [3]=0.15 }
             },
         },
     },
         
 })
-
+minetest.register_craft({
+    output = "nitro_digger:nitro_digger 1",
+    recipe = {
+        {"default:pick_diamond", "default:pick_diamond"                       },
+        {"default:pick_diamond", "default:pick_diamond"},
+        {"default:pick_diamond", "default:pick_diamond"}
+    }
+})
 

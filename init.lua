42e86b4928982273e62da0b68b2a9a6b45ecdb20
nri <noahrobert03@gmail.com>
2019-06-03 17:07:41 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,46 +1,90 @@
 minetest.register_tool("nitro_digger:nitro_digger", {
+
     description = "Nitro Digger",
+
     inventory_image = "nitro_digger.png",
+
     tool_capabilities = {
+
         full_punch_interval = 0,
+
         max_drop_level = 1,
+
         groupcaps = {
-        cracky = {
- 		    maxlevel = 3,
-		    uses = 900,
-		    times = { [1]=0.1, [2]=0.1, [3]=0.1 }	
+
+            cracky = {
+
+		maxlevel = 3,
+
+		uses = 900,
+
+		times = { [1]=0.1, [2]=0.1, [3]=0.1 }	
+
 	    },				
-        crumbly = {
-            maxlevel = 3,
-            uses = 900,
-            times = { [1]=0.1, [2]=0.1, [3]=0.1 }
-        },
-        snappy = {
-            maxlevel = 3,
-            uses = 900,
-            times = { [1]=0.1, [2]=0.1, [3]=0.1 }
-        },
+
+            crumbly = {
+
+                maxlevel = 3,
+
+                uses = 900,
+
+                times = { [1]=0.1, [2]=0.1, [3]=0.1 }
+
+            },
+
+            snappy = {
+
+                maxlevel = 3,
+
+                uses = 900,
+
+                times = { [1]=0.1, [2]=0.1, [3]=0.1 }
+
+            },
+
 	    choppy = {
-            maxlevel = 3,
-            uses = 900,
-            times = { [1]=0.15, [2]=0.15, [3]=0.15 }
+
+                maxlevel = 3,
+
+                uses = 900,
+
+                times = { [1]=0.15, [2]=0.15, [3]=0.15 }
+
+            },
+
+	    fleshy = {
+
+                maxlevel = 3,
+
+                uses = 900,
+
+                times = { [1]=0.1, [2]=0.1, [3]=0.1 }
+
+            },
+
+	   explody = {
+
+                maxlevel = 3,
+
+                uses = 900,
+
+                times = { [1]=0.1, [2]=0.1, [3]=0.1 }
+
+            },
+
+	    oddly_breakable_by_hand = {
+
+                maxlevel = 3,
+
+                uses = 900,
+
+                times = { [1]=0.1, [2]=0.1, [3]=0.1 }
+
+            },
+
         },
- 	    fleshy = {
-            maxlevel = 3,
-            uses = 900,
-            times = { [1]=0.1, [2]=0.1, [3]=0.1 }
-        },
- 	   explody = {
-           maxlevel = 3,
-           uses = 900,
-           times = { [1]=0.1, [2]=0.1, [3]=0.1 }
-        },
- 	    oddly_breakable_by_hand = {
-            maxlevel = 3,
-            uses = 900,
-            times = { [1]=0.1, [2]=0.1, [3]=0.1 }
-        },
-     },
+
+    },
     on_use = function(itemstack, user, pointed_thing)
         local pos = minetest.get_pointed_thing_position(pointed_thing, nil);
         if not minetest.is_protected(pos, user:get_player_name()) then
@@ -48,12 +92,21 @@ minetest.register_tool("nitro_digger:nitro_digger", {
         end
     end,
 })
-minetest.register_craft({
-    output = "nitro_digger:nitro_digger 1",
-    recipe = {
-        {"default:pick_diamond", "default:pick_diamond"                       },
-        {"default:pick_diamond", "default:pick_diamond"},
-        {"default:pick_diamond", "default:pick_diamond"}
-    }
-})
 
+
+
+minetest.register_craft({
+
+    output = "nitro_digger:nitro_digger 1",
+
+    recipe = {
+
+        {"default:pick_diamond", "default:pick_diamond"                       },
+
+        {"default:pick_diamond", "default:pick_diamond"},
+
+        {"default:pick_diamond", "default:pick_diamond"}
+
+    }
+
+})
